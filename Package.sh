e800d225e4b3c3521c87456b53af1fbabe315388
John Logostini <johnlogostini16485@gmail.com>
2022-11-21 07:57:55 -0400

    Updated the package script from LEGO Batman 2 to LEGO Batman 3

--- Package.sh
+++ Package.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-mkdir .temp-253476
-cp -r assets/ .temp-253476
-cp -r tools/ .temp-253476
-cp config.json .temp-253476
-cd .temp-253476
-zip -r LEGO_Batman_2_DC_Super_Heroes.integration assets/ tools/ config.json
+mkdir .temp-963601
+cp -r assets/ .temp-963601
+cp -r tools/ .temp-963601
+cp config.json .temp-963601
+cd .temp-963601
+zip -r LEGO_Batman_3_Beyond_Gotham.integration assets/ tools/ config.json
 cd ../
 mkdir Package
-mv .temp-253476/LEGO_Batman_2_DC_Super_Heroes.integration Package/
-rm -Rf .temp-253476
+mv .temp-963601/LEGO_Batman_3_Beyond_Gotham.integration Package/
+rm -Rf .temp-963601
